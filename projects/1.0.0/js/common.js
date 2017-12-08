c2cac3dcb0f0d5fd6807b7398cf023c5b834b3f1
yjc <yjc@8cd2c64c-1b4d-0343-912b-3b636444da1f>
2017-12-08 12:44:59 +0000

    git-svn-id: https://39.108.144.9:8443/svn/xhweb@313 8cd2c64c-1b4d-0343-912b-3b636444da1f

--- projects/1.0.0/js/common.js
+++ projects/1.0.0/js/common.js
@@ -429,16 +429,21 @@ function is_weixn(){
       Num = Num < 1 ? 1 : Num;
     } 
     time = setInterval(function(){
+    if(Num < 0.01){
+      clearInterval(time);
+      console.log(parseFloat(lastNum))
+      obj.text(parseFloat(lastNum).toFixed(2));
+      return false;
+    }
       firstNum = parseFloat(firstNum)+Num > parseFloat(lastNum) ? parseFloat(lastNum) : parseFloat(firstNum)+Num;
       firstNum = is_toFixed ? firstNum.toFixed(2) : firstNum.toFixed(0);
-
       obj.text(firstNum);
       if(firstNum == parseFloat(lastNum)){
         clearInterval(time)
       }
     },1)
-
   }
+
 
 //提示
 var fb_error ={
@@ -548,3 +553,30 @@ $(function(){
     return false;
   })
 })
+
+//微信注入
+function fb_config(){
+   $.getJSON(locahost +'wechat/getConfig',{"url":location.href},function(data) {
+        if(data.code == 200){
+          wx.config({
+          debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId: data.data.appId, // 必填，公众号的唯一标识
+          timestamp:data.data.timestamp , // 必填，生成签名的时间戳
+          nonceStr:data.data.nonceStr, // 必填，生成签名的随机串
+          signature: data.data.signature,// 必填，签名，见附录1
+          jsApiList: ["onMenuShareAppMessage","onMenuShareTimeline","openAddress","scanQRCode"],// 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+          // url:decodeURIComponent(data.data.url)
+        });
+      }
+    })
+}
+$(function(){
+  try {
+    if (wx && is_weixn()) {
+       fb_config();
+      }
+    } catch (e) {
+       
+    }
+})
+//微信注入
